a722a84278b6941697e15b0d38a77109002cb904
Mayank <mayankmchandratre@gmail.com>
2024-12-26 11:32:36 +0530

    test-message

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -29,11 +29,11 @@ export function CodeBlock({
 
   if (!inline) {
     return (
-      <div className="not-prose flex flex-col">
+      <div className="not-prose flex flex-col ">
         {tab === 'code' && (
           <pre
             {...props}
-            className={`text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900`}
+            className={`text-sm w-full overflow-x-auto bg-gray-200 dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900`}
           >
             <code className="whitespace-pre-wrap break-words">{children}</code>
           </pre>
